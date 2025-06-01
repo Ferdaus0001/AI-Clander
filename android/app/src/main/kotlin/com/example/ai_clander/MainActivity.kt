5a5819d16e4d7d7130306cc41b90cbdf385d54f7
ferdaus <ferdaushossan7788990@gmail.com>
2025-06-01 16:13:46 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/ai_clander/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ai_clander
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/presentation/clander_screen.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+
+class MyCalendarPage extends StatefulWidget {
+  const MyCalendarPage({super.key});
+
+  @override
+  _MyCalendarPageState createState() => _MyCalendarPageState();
+}
+
+class _MyCalendarPageState extends State<MyCalendarPage> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF9F9F9),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(180),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          flexibleSpace: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset(
+                getSeasonImage(_focusedDay.month),
+                fit: BoxFit.cover,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.black.withOpacity(0.9), Colors.transparent],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    'My Calendar',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          Card(
+            color: Colors.white,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            elevation: 16,
+            shadowColor: Colors.black26,
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: TableCalendar(
+                currentDay: DateTime.now(),
+                calendarFormat: CalendarFormat.month,
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                locale: 'en_US',
+                dayHitTestBehavior: HitTestBehavior.deferToChild,
+                  availableCalendarFormats: const {
+                    CalendarFormat.month: 'Month',
+                    CalendarFormat.twoWeeks: '2 Weeks',
+                    CalendarFormat.week: '1 Week',
+                  },
+                daysOfWeekHeight: 40,
+                daysOfWeekStyle: DaysOfWeekStyle(
+                  weekdayStyle: const TextStyle(
+                    color: Colors.black87,
+
+                    fontWeight: FontWeight.bold,
+                  ),
+                  weekendStyle: const TextStyle(
+                    color: Colors.redAccent,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                daysOfWeekVisible: true,
+                enabledDayPredicate: (day) {
+                  return true; //All days selectable
+                },
+                formatAnimationCurve: Curves.easeInOut,
+                formatAnimationDuration: const Duration(milliseconds: 300),
+                rowHeight: 60,
+                weekendDays: const [DateTime.saturday, DateTime.sunday],
+                firstDay: DateTime(2024, 1, 1),
+                lastDay: DateTime(2050, 12, 31),
+                focusedDay: _focusedDay,
+                selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Selected: ${selectedDay.toLocal()}")),
+                  );
+                },
+                calendarStyle: CalendarStyle(
+                  outsideDaysVisible: false,
+                  isTodayHighlighted: true,
+                  todayDecoration: BoxDecoration(
+                    color: Colors.orange.shade600,
+                    shape: BoxShape.circle,
+                  ),
+                  selectedDecoration: BoxDecoration(
+                    color: Colors.blue.shade600,
+                    shape: BoxShape.circle,
+                  ),
+                  defaultTextStyle: const TextStyle(
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  weekendTextStyle: const TextStyle(
+                    color: Colors.redAccent,
+                  ),
+                ),
+                calendarBuilders: CalendarBuilders(
+                  defaultBuilder: (context, day, focusedDay) {
+                    if (day.weekday == DateTime.friday) {
+                      //Wednesday – Default Color
+                      return null;
+                    } else {
+                      // Color of other days
+                      return Container(
+                        margin: const EdgeInsets.all(6.0),
+                        alignment: Alignment.center,
+                        child: Text(
+                          '${day.day}',
+                          style: const TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// 🔁 Helper: Get image based on month
+  String getSeasonImage(int month) {
+    if ([12, 1, 2].contains(month)) {
+      return 'assets/image/R.jpg';
+    } else if ([3, 4, 5].contains(month)) {
+      return 'assets/image/R.jpg';
+    } else if ([6, 7, 8].contains(month)) {
+      return 'assets/image/R.jpg';
+    } else {
+      return 'assets/image/R.jpg';
+    }
+  }
+}
